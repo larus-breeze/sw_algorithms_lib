c0f9eb7e105d660a56d5c5eb0477a0054445673e
realtimepeople <klaus.schaefer@h-da.de>
2022-09-10 17:20:48 +0200

    Cleanup.

--- NAV_Algorithms/flight_observer.cpp
+++ NAV_Algorithms/flight_observer.cpp
@@ -40,11 +40,8 @@ void flight_observer_t::update (
       windspeed_instant_observer.update( gnss_velocity - air_velocity, heading_vector, circle_state);
 
       // non TEC compensated vario, negative if *climbing* !
-#if GNSS_VERTICAL_SPEED_INVERTED
-      vario_uncompensated_GNSS = - KalmanVario_GNSS.update ( GNSS_negative_altitude, - gnss_velocity.e[DOWN], ahrs_acceleration.e[DOWN]);
-#else
       vario_uncompensated_GNSS = - KalmanVario_GNSS.update ( GNSS_negative_altitude, gnss_velocity.e[DOWN], ahrs_acceleration.e[DOWN]);
-#endif
+
       float speed_compensation =
     		  (
     		      ((gnss_velocity.e[NORTH] - wind_average.e[NORTH]) * gnss_acceleration.e[NORTH]) +
@@ -53,7 +50,6 @@ void flight_observer_t::update (
     		   ) * RECIP_GRAVITY;
 
       speed_compensation_GNSS = speed_compensation_fusioner.respond( speed_compensation, speed_compensation_TAS);
-//      speed_compensation_GNSS = speed_compensation;
       vario_averager_GNSS.respond( speed_compensation_GNSS + vario_uncompensated_GNSS);
     }
 }
