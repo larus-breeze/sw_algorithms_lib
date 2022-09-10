e417c0c29cdafff8d13e0338d6ce3205a0058500
realtimepeople <klaus.schaefer@h-da.de>
2022-09-10 18:08:29 +0200

    Added license information. + Comments.

--- Output_Formatter/generic_CAN_driver.h
+++ Output_Formatter/generic_CAN_driver.h
@@ -1,9 +1,26 @@
-/*
- * generic_CAN_driver.h
- *
- *  Created on: Sep 5, 2022
- *      Author: schaefer
- */
+/***********************************************************************//**
+ * @file		generic_CAN_driver.h
+ * @brief		Portable interface to some CAN-bus driver
+ * @author		Dr. Klaus Schaefer
+ * @copyright 		Copyright 2021 Dr. Klaus Schaefer. All rights reserved.
+ * @license 		This project is released under the GNU Public License GPL-3.0
+
+    <Larus Flight Sensor Firmware>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ **************************************************************************/
 
 #ifndef GENERIC_CAN_DRIVER_H_
 #define GENERIC_CAN_DRIVER_H_
@@ -83,6 +100,7 @@ public:
 
 #pragma pack(pop)
 
+//! Global CAN send procedure
 bool CAN_send( const CANpacket &p, unsigned dummy);
 
 #endif /* GENERIC_CAN_DRIVER_H_ */
