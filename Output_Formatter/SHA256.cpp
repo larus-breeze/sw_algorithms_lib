3f754a90f193cbcad5d00b7cd67978879c7bf49b
realtimepeople <klaus.schaefer@h-da.de>
2024-02-20 16:43:31 +0100

    Compiles, untested.

--- Output_Formatter/SHA256.cpp
+++ Output_Formatter/SHA256.cpp
@@ -1,9 +1,24 @@
 #include "SHA256.h"
-#include <cstring>
-#include <sstream>
-#include <iomanip>
 
-constexpr std::array<uint32_t, 64> SHA256::K;
+ROM uint32_t SHA256::K[64] =
+    {
+	0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,
+	0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+	0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,
+	0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
+	0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,
+	0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+	0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,
+	0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
+	0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,
+	0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
+	0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,
+	0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+	0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,
+	0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
+	0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,
+	0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+    };
 
 SHA256::SHA256(): m_blocklen(0), m_bitlen(0) {
 	m_state[0] = 0x6a09e667;
@@ -29,17 +44,10 @@ void SHA256::update(const uint8_t * data, size_t length) {
 	}
 }
 
-void SHA256::update(const std::string &data) {
-	update(reinterpret_cast<const uint8_t*> (data.c_str()), data.size());
-}
-
-std::array<uint8_t,32> SHA256::digest() {
-	std::array<uint8_t,32> hash;
-
+void SHA256::make_digest(  uint8_t * hash)
+{
 	pad();
 	revert(hash);
-
-	return hash;
 }
 
 uint32_t SHA256::rotr(uint32_t x, uint32_t n) {
@@ -115,10 +123,12 @@ void SHA256::pad() {
 		m_data[i++] = 0x00; // Pad with zeros
 	}
 
-	if(m_blocklen >= 56) {
+	if(m_blocklen >= 56)
+	  {
 		transform();
-		memset(m_data, 0, 56);
-	}
+		for( unsigned i=0; i<56; ++i)
+		  m_data[i]=0;
+	  }
 
 	// Append to the padding the total message's length in bits and transform.
 	m_bitlen += m_blocklen * 8;
@@ -133,23 +143,15 @@ void SHA256::pad() {
 	transform();
 }
 
-void SHA256::revert(std::array<uint8_t, 32> & hash) {
+void SHA256::revert( uint8_t * hash)
+{
 	// SHA uses big endian byte ordering
 	// Revert all bytes
-	for (uint8_t i = 0 ; i < 4 ; i++) {
-		for(uint8_t j = 0 ; j < 8 ; j++) {
+	for (uint8_t i = 0 ; i < 4 ; i++)
+	  {
+		for(uint8_t j = 0 ; j < 8 ; j++)
+		  {
 			hash[i + (j * 4)] = (m_state[j] >> (24 - i * 8)) & 0x000000ff;
-		}
-	}
-}
-
-std::string SHA256::toString(const std::array<uint8_t, 32> & digest) {
-	std::stringstream s;
-	s << std::setfill('0') << std::hex;
-
-	for(uint8_t i = 0 ; i < 32 ; i++) {
-		s << std::setw(2) << (unsigned int) digest[i];
-	}
-
-	return s.str();
+		  }
+	  }
 }
