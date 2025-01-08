89a623bbcc7b6ced5c97406d73203649457cf09b
realtimepeople <klaus.schaefer@h-da.de>
2025-01-08 10:16:36 +0100

    Bugfix merge completed.

--- NAV_Algorithms/air_density_observer.h
+++ NAV_Algorithms/air_density_observer.h
@@ -58,11 +58,7 @@ public:
 };
 
 //! Measures air density and reference pressure
-<<<<<<< HEAD
 class air_density_observer_t
-=======
-class air_density_observer
->>>>>>> refs/remotes/origin/soft_iron_correction
 {
 public:
   air_density_observer_t (void)
