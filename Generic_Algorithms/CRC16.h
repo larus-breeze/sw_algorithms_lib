1fa70135149ddedc2f80ceb43cb15dabd368cc8e
realtimepeople <klaus.schaefer@h-da.de>
2025-12-20 16:44:41 +0100

    Moved CRC16 mechanism into the lib.

---
+++ Generic_Algorithms/CRC16.h
@@ -0,0 +1,37 @@
+#ifndef CRC16_H_
+#define CRC16_H_
+
+#include <CRC16.h>
+#include <stdint.h>
+#include "embedded_memory.h"
+
+extern ROM uint16_t CRCtable[];
+
+static inline uint16_t CRC16( const uint16_t input, uint16_t crc)
+{
+    uint8_t carry=(crc >> 8) ^ input;
+    crc = (crc << 8) ^ CRCtable[carry];
+	return crc;
+}
+
+static inline uint16_t CRC16_blockcheck_bytes( const uint8_t *input, uint16_t length)
+{
+  uint16_t crc=0;
+  while( length--)
+    {
+      crc = CRC16( (const uint16_t)(*input++), crc);
+    }
+  return crc;
+}
+
+static inline uint16_t CRC16_blockcheck( const uint16_t *input, uint16_t length)
+{
+  uint16_t crc=0;
+  while( length--)
+    {
+      crc = CRC16( *input++, crc);
+    }
+  return crc;
+}
+
+#endif /* CRC16_H_ */
