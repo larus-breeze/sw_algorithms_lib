13a81eec2a3c5855bdb9423303405ca2f82a6acf
realtimepeople <klaus.schaefer@h-da.de>
2026-02-12 12:39:00 +0100

    Warnings removed.

--- Persistent_Data/flexible_log_file.cpp
+++ Persistent_Data/flexible_log_file.cpp
@@ -7,7 +7,7 @@ bool flexible_log_file_t::append_record ( flexible_log_file_record_type type, ui
   uint32_t block_identifier;
   uint32_t size;
   uint32_t long_size;
-  unsigned long_identifier = 0;
+  uint32_t long_identifier = 0;
   if( type > 254 || data_size_words > 254) // in this case we use two more words for identifier and length
     {
       block_identifier = 0;
@@ -29,7 +29,7 @@ bool flexible_log_file_t::append_record ( flexible_log_file_record_type type, ui
   write_block( &block_identifier, 1);
   if( long_identifier != 0)
     {
-      write_block( &long_identifier, 1);
+      write_block( (uint32_t *)&long_identifier, 1);
       write_block( &long_size, 1);
     }
   write_block( data, data_size_words);
@@ -40,7 +40,7 @@ unsigned flexible_log_file_t::verify_record_get_size( uint32_t block_identifier)
 {
   uint32_t type = block_identifier & 0xff;
   uint32_t size = (block_identifier & 0xff00) >> 8;
-  uint16_t info = (size << 8) | type;
+  uint16_t info = (uint16_t)((size << 8) | type);
   uint32_t crc_computed = CRC16( info, CRC_SEED);
   if( crc_computed != (block_identifier >> 16))
     return 0; // wrong CRC !
