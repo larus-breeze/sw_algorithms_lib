1e3b5b9586c11c42f3be054a157099fa70fd702c
realtimepeople <klaus.schaefer@h-da.de>
2026-03-20 13:22:20 +0100

    Added range check for ID.

--- Persistent_Data/persistent_data_file.h
+++ Persistent_Data/persistent_data_file.h
@@ -120,6 +120,8 @@ public:
 
   EEPROM_file_system_node * find_datum( EEPROM_file_system_node::ID_t id)
   {
+    ASSERT( id > 0);
+    ASSERT( id < size);
     return registry[id]; // may be zero
   }
 
