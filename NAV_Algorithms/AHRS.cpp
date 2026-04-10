c257707e8a8a2742f5d58f04dbe89fbf89b80fbb
realtimepeople <klaus.schaefer@h-da.de>
2026-04-10 18:02:15 +0200

    AHRS attitude setup honorizes magnetic declination.

--- NAV_Algorithms/AHRS.cpp
+++ NAV_Algorithms/AHRS.cpp
@@ -73,6 +73,11 @@ AHRS_type::attitude_setup (
 
   float3matrix coordinates ( fcoordinates);
   attitude.from_rotation_matrix ( coordinates);
+
+  quaternion<float>decl_comp;
+  decl_comp.from_euler( ZERO, ZERO, ATAN2( expected_nav_induction[EAST],expected_nav_induction[NORTH]));
+  attitude = attitude * decl_comp;
+
   attitude.get_rotation_matrix (body2nav);
   euler = attitude;
 }
